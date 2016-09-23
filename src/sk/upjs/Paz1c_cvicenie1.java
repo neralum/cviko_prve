9659a63e33cdc50fe625b30b506a6878965e89f6
student <student@158.197.35.81>
2016-09-23 07:46:03 +0200

    pridanie prveho testovacieho projektu + vypis

---
+++ src/sk/upjs/Paz1c_cvicenie1.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package sk.upjs;
+
+/**
+ *
+ * @author student
+ */
+public class Paz1c_cvicenie1 {
+
+    /**
+     * @param args the command line arguments
+     */
+    
+    
+    public static void main(String[] args) {
+    
+        System.out.println("Test project for Ghub");
+        
+    }
+    
+}
